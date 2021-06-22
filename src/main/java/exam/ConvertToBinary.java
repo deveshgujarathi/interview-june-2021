d84348b54619f7e9ed0e9512025333b68f94cd17
Devesh Gujarathi <70016314+deveshgujarathi@users.noreply.github.com>
2021-06-22 13:19:44 +0530

    Update ConvertToBinary.java

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -1,9 +1,22 @@
 package exam;
+import java.util.*;
 
 public class ConvertToBinary {
 
-  public String toBinary(final int n) {
+  public void toBinary(int n) {
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return "";
+    int b[] = new int[40];
+     int index = 0;
+     while(n > 0){
+       b[index++] = n%2;
+       n = n/2;
+     }
+     for(int i = index-1;i >= 0;i--){
+       System.out.print(b[i]);
+     }
+}
+  public static void main(String a[]){
+    ConvertToBinary obj = new ConvertToBinary();
+    obj.toBinary(35);
   }
 }
